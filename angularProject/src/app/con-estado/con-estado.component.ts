2898a9ad36e3cadf7c1113cb9a2b654397e5464f
rama8810 <ing.rama88@gmail.com>
2023-10-03 23:19:10 -0500

    actualizacion

--- angularProject/src/app/con-estado/con-estado.component.ts
+++ angularProject/src/app/con-estado/con-estado.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Producto } from '../interfaces/producto';
+import { Tienda } from '../models/tienda.mod';
 
 @Component({
   selector: 'app-con-estado',
   templateUrl: './con-estado.component.html',
   styleUrls: ['./con-estado.component.scss']
 })
-export class ConEstadoComponent {
+export class ConEstadoComponent implements OnInit {
 
+  modeloTienda: Tienda = new Tienda();
+  itemsComprados: Array<Producto>;
+
+  constructor() {}
+
+  ngOnInit() {}
 }
