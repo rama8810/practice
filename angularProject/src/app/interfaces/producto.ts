a0ffab02314fb33273b24725d1d876ca95724942
rama8810 <ing.rama88@gmail.com>
2023-10-03 23:27:38 -0500

    startAgain

--- angularProject/src/app/interfaces/producto.ts
+++
@@ -1,6 +0,0 @@
-export interface Producto {
-  precio?: number;
-  titulo?: string;
-  imagen?: string;
-  texto?: string;
-}
--- angularProject/src/app/models/tienda.mod.spec.ts
+++
@@ -1,7 +0,0 @@
-import { TiendaMod } from './tienda.mod';
-
-describe('TiendaMod', () => {
-  it('should create an instance', () => {
-    expect(new TiendaMod()).toBeTruthy();
-  });
-});
